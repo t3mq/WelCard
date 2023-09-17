e06a70fd48d324c07482673e6c723dbaa8242179
ZqxDev <78566312+ZqxDev@users.noreply.github.com>
2023-09-17 08:58:49 +0200

    Update test.js

--- exemple/test.js
+++ exemple/test.js
@@ -15,3 +15,20 @@
     fs.writeFileSync("card.png", cardBuffer);
     console.log("done")
 })()
+
+// Now Direct Immersion With A Bot Discord
+
+const { welCard } = require("welcard");
+
+let Car = new welCard()
+        .setName("Bienvenue")
+        .setAuthor("Zqx_")
+        .setServer("Sur 🪐| LunarWave")
+        .setColor("auto")
+        .setBrightness(50)
+        .setThumbnail("https://cdn.discordapp.com/avatars/359924289832484865/dae79e262689070227d7ee2c72b20ae9.png?size=4096")
+       
+    const cardBuffer = await card.build();
+    const channel = client.channels.cache.get("ID CHANNEL");
+
+    channel.send({content: `Welcome ${member} on ${member.guild.name}`, files: [cardBuffer]});
